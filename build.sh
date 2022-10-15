30365d621beb223433470f9415f85636888691fc
zegolem <zegolem1383@outlook.fr>
2022-10-15 13:55:00 +0200

    build.sh: Can now build optimized binaries
    Also, it now respects many environment variables

--- build.sh
+++ build.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
 
 set -ex;
-clang -glldb -fsanitize=address,undefined -std=c2x -Wall -Wextra wf.c -o wf;
+
+CC=${CC:-clang}
+CFLAGS="$CFLAGS -std=c2x"
+DEBUGGER=${DEBUGGER:-lldb}
+STRIP=${STRIP:-llvm-strip}
+
+if [ "$1" = 'dev' ]; then
+	$CC -g$DEBUGGER -fsanitize=address,undefined -Wall -Wextra $CFLAGS wf.c -o wf;
+else
+	$CC -O2 $CFLAGS wf.c -o wf;
+	$STRIP wf
+fi;
